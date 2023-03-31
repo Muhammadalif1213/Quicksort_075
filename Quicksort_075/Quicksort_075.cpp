6e1e9d30f51516e4c1831205c59b32db1e0e1c79
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-03-31 20:23:53 +0700

    memberikan perintah untuk memasukan jumlah array maximal 20

--- Quicksort_075/Quicksort_075.cpp
+++ Quicksort_075/Quicksort_075.cpp
@@ -8,4 +8,25 @@ int mov_count = 0; //number of data movement
 int n;
 
 void input() {
+    while (true)
+    {
+        cout << "masukan panjang element array :";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMasukan panjang array adalah 20" << endl;
+    }
+
+    cout << "\n====================" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n====================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
 }
+
