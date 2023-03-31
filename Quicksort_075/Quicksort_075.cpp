a3c85dca632050fc8e0fdaa146e6cdc2fac811e0
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-03-31 22:22:29 +0700

    menambahkan perintah untuk menampilkan hasil perhitungan

--- Quicksort_075/Quicksort_075.cpp
+++ Quicksort_075/Quicksort_075.cpp
@@ -94,4 +94,15 @@ void q_short(int low, int high)
 }
 
 void display() {
+    cout << "=============" << endl;
+    cout << "sorted array" << endl;
+    cout << "=============" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nNumber of comparasions :" << cmp_count << endl;
+    cout << "number of data movements :" << mov_count << endl;
 }
